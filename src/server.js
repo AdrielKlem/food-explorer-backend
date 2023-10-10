7a52a85209e81f6b2265eda65b73598bdd93f124
Adriel Klem <adrielklem16@gmail.com>
2023-10-10 13:40:58 -0300

    add env

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")
 require("express-async-errors")
 const migrationsRun = require("./database/knex/index.js")
 const AppError = require("./utils/AppError")
@@ -34,6 +35,6 @@ app.use((error, request, response, next) => {
     })
 })
 
+const PORT = process.env.PORT || 3333;
 
-const PORT = 3333
 app.listen(PORT, ()=> console.log(`Server is running on PORT ${PORT}`))
